08913eaa5b373bb71621d38a95aec8af55cab4dd
calvin728 <calvin@Skelley.us>
2026-01-11 15:27:11 -0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,10 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --medvram
+
+git config --global --add safe.directory C:/Users/Calvi/stable-diffusion-webui
+
+git pull
 
 call webui.bat
